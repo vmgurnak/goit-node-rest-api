cbea7dc5f20b4a1d785f0868b44af3bfd7a5f9ac
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-06-09 10:47:31 +0300

    updatet

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', authMiddleware, contactsRouter);
-app.use('/api/users', authRoutes);
+app.use('/users', authRoutes);
 
 app.use((_, res) => {
   res.status(404).json({ message: 'Route not found' });
